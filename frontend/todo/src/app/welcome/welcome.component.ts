f9d9724ff011cc2158342ad0908094c9fa0b6f8b
Grant Kohler <grant.kohler@gmail.com>
2019-08-11 21:37:04 -0400

    Data Binding
    Inplemented data binding in the class

---
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  message = 'Some Welcome Message'
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.message)
+  }
+
+}
