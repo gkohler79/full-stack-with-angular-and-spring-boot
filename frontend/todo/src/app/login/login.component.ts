ed81613a8da28c399fd8aa1adaa8c7d61aa0d396
Grant Kohler <grant.kohler@gmail.com>
2019-08-23 10:28:25 -0400

    Through Step 27
    Added the bootstrap framework and a header and footer

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,6 +22,7 @@ export class LoginComponent implements OnInit {
 
     //console.log(this.username);
     if(this.username === 'in28minutes' && this.password === 'dummy') {
+      this.router.navigate(['welcome', this.username])
       this.invalidLogin = false
     } else {
       this.invalidLogin = true
